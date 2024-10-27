2656738971463e26e51a813a5393a1c4e86e39a3
Nikita Tsomuk <tsomuk@me.com>
2024-10-27 11:27:10 +0200

    Initial Commit

---
+++ FlightChecker/FlightCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightCheckerApp.swift
+//  FlightChecker
+//
+//  Created by Nikita Tsomuk on 27.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct FlightCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
